5811f2cc01f6657a7e39e9089f9f97c526e9d725
Cs4K1Sr4C <cs4k1sr4c@tutanota.com>
2023-04-17 15:18:05 +0200

    Remove unnecessary codes

--- app/src/App.js
+++ app/src/App.js
@@ -4,26 +4,9 @@ import React, { useEffect, useState } from "react";
 // UI
 import "./index.css";
 import "rsuite/dist/rsuite.min.css";
-import {
-  Container,
-  Header,
-  Sidebar,
-  Content,
-  Navbar,
-  Nav,
-  Button,
-  SelectPicker,
-} from "rsuite";
-import HomeIcon from "@rsuite/icons/legacy/Home";
-import CogIcon from "@rsuite/icons/legacy/Cog";
 
 // Router handler
 import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
-
-// Language handler modules
-import { useTranslation } from "react-i18next";
-import i18n from "i18next";
-import "./i18n";
 
 // Parts
 import Game from "./components/Game2.jsx";
