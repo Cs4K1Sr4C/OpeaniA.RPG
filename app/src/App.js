2433534a4ee3ff2b4c12b763da916efae869f755
Cs4K1Sr4C <cs4k1sr4c@tutanota.com>
2023-04-17 15:05:31 +0200

    Simple modification

--- app/src/App.js
+++ app/src/App.js
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 
 // UI
-import "./App.css";
+import "./index.css";
 import "rsuite/dist/rsuite.min.css";
 import {
   Container,
@@ -25,86 +25,10 @@ import { useTranslation } from "react-i18next";
 import i18n from "i18next";
 import "./i18n";
 
-// Language switcher functional component
-import LanguageSwitcher from "./components/LanguageSwitcher";
-
 // Parts
 import Game from "./components/Game2.jsx";
-import CharacterDrawer from "./components/CharacterDrawer";
-
-/*function _Header() {
-  return (<Header>
-    <Navbar
-      appearance="inverse"
-      style={{
-        backgroundColor: "black",
-        boxShadow: "0px 0px 10px black",
-        position: "relative",
-      }}
-    >
-      <Nav pullRight>
-        <Nav.Item>
-          <SelectPicker
-            data={selectableLanguages}
-            style={{ width: 224 }}
-            value={currentLanguage}
-            onChange={(value) => {
-              handleLanguageChange(value);
-            } } />
-        </Nav.Item>
-        <Nav.Item>
-          <Button
-            style={{ backgroundColor: "red", color: "white" }}
-            onClick={() => {
-              localStorage.removeItem("character");
-              document.location.reload();
-            } }
-          >
-            {t("RESET_GAME")}
-          </Button>
-        </Nav.Item>
-      </Nav>
-    </Navbar>
-  </Header>)
-}*/
 
 function App() {
-  const { t } = useTranslation();
-  const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng);
-  };
-  const [currentLanguage, setCurrentLanguage] = useState(i18n.language);
-  const handleLanguageChange = (value) => {
-    const language = value;
-    i18n.changeLanguage(language);
-    setCurrentLanguage(language);
-  };
-  const selectableLanguages = [
-    { label: "English", value: "en" },
-    { label: "Hungarian", value: "hu" },
-    { label: "Español", value: "es" },
-    { label: "Français", value: "fr" },
-  ].map((item) => ({ label: item.label, value: item.value }));
-
-  const buy_me_a_coffe_link = () => {
-    return `https://img.buymeacoffee.com/button-api/?text=${t(
-      "BUY_ME_A_COFFE"
-    )}&emoji=&slug=cs4k1sr4c&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff`;
-  };
-
-  const [character, setCharacter] = useState({});
-  const [characterExists, setCharacterExists] = useState(false);
-
-  useEffect(() => {
-    const existingCharacter = localStorage.getItem("character");
-
-    if (existingCharacter) {
-      console.log("Character already exists", JSON.parse(existingCharacter));
-      setCharacter(JSON.parse(existingCharacter));
-      setCharacterExists(true);
-    }
-  }, []);
-
   return (
     <BrowserRouter>
       <Routes>
